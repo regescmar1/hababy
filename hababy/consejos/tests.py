018dbd5903ecb41bb0a526c3df856e7dcb3f83cc
regescmar1 <regescmar1@alum.us.es>
2024-07-01 02:29:33 +0200

    Correcciones calidad

--- hababy/consejos/tests.py
+++ hababy/consejos/tests.py
@@ -13,7 +13,6 @@ class ConsejosViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'consejos.html')
 
-
     def test_nauseas(self):
         user = User.objects.create_user(username='testuser', email='test@example.com', password='password123')
         self.client.force_login(user)
@@ -21,15 +20,12 @@ class ConsejosViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'nauseas.html')
 
-
-    
     def test_ejercicios(self):
         user = User.objects.create_user(username='testuser', email='test@example.com', password='password123')
         self.client.force_login(user)
         response = self.client.get(reverse('ejercicios'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'ejercicios.html')
-
 
     def test_nutricion(self):
         user = User.objects.create_user(username='testuser', email='test@example.com', password='password123')
@@ -38,14 +34,12 @@ class ConsejosViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'nutricion.html')
 
-
     def test_lactancia(self):
         user = User.objects.create_user(username='testuser', email='test@example.com', password='password123')
         self.client.force_login(user)
         response = self.client.get(reverse('lactancia'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'lactancia.html')
-
 
     def test_bolso_hospital(self):
         user = User.objects.create_user(username='testuser', email='test@example.com', password='password123')
@@ -68,7 +62,6 @@ class ConsejosViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'induccion_al_parto.html')
 
-
     def test_cordon_umbilical(self):
         user = User.objects.create_user(username='testuser', email='test@example.com', password='password123')
         self.client.force_login(user)
@@ -76,18 +69,9 @@ class ConsejosViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'cordon_umbilical.html')
 
-
     def test_suelo_pelvico(self):
         user = User.objects.create_user(username='testuser', email='test@example.com', password='password123')
         self.client.force_login(user)
         response = self.client.get(reverse('suelo_pelvico'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'suelo_pelvico.html')
-    
-
-
-
-    
-
-
-
--- hababy/educacion_maternal/views.py
+++ hababy/educacion_maternal/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-
 from educacion_maternal.forms.forms import SesionForm
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
